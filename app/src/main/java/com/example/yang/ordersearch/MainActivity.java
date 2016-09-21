5d41fc2fced0231db0cd30cd3d1b286f81cda9b2
Deardear099 <thankyousoblue@sina.cn>
2016-09-21 22:02:21 +0800

    init commit

---
+++ app/src/main/java/com/example/yang/ordersearch/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yang.ordersearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.work);
+    }
+}
